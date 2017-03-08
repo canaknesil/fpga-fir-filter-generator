712e46a69149175a583288f2e317e0759b3d9a77
Can Aknesil <caknesil13@ku.edu.tr>
2017-03-08 15:04:23 +0200

    Tested raspberry gpio with led.

---
+++ test/gpio-led.c
@@ -0,0 +1,30 @@
+
+#include <stdio.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <unistd.h>
+
+int main() {
+    
+    int fdgpio = open("/dev/gpiomem", O_RDWR);
+    if (fdgpio < 0) {
+        printf("Error opening /dev/gpiomem\n");
+        return -1;
+    }
+
+    unsigned int *gpio = (unsigned int *) mmap(0, 4096, PROT_READ+PROT_WRITE, MAP_SHARED, fdgpio, 0);
+
+	
+	gpio[0] = 0b001001001001001001001001001001;
+    gpio[7] = 0;
+	
+	while (1) {
+		sleep(1);
+		gpio[7] = 1<<2;
+		sleep(1);
+		gpio[10] = 1<<2;
+	}
+
+
+    return 0;
+}
