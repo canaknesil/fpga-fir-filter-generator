0610dfe41338ec6a6e4c5775e188a0f144ed1871
Can Aknesil <canaknesil@debian-mac>
2017-05-09 13:32:38 +0300

    Added clock pulse to filtering.

--- src/fpga-causal-fir-filter-pi.cpp
+++ src/fpga-causal-fir-filter-pi.cpp
@@ -11,17 +11,56 @@ using namespace std;
 #include <iostream>
 #include <vector>
 
-
+using namespace FPGA_Comm;
 using namespace std;
 
 
+
 vector<char> FpgaCausalFirFilter::filter(const vector<char> &input) {
-    //TODO using fpga-comm.h
-    cout << "FpgaCausalFirFilter::filter(): Not implemented yet." << endl;
+    
+    return this->filter(input, 0);
+
+}
+
+
+
+vector<char> FpgaCausalFirFilter::filter(const vector<char> &input, int filterSize) {
+    
     vector<char> output;
+    
+    if (initialize() != 0) {
+        cout << "FpgaCausalFirFilter::filter(): failed to initialize wiringPi" << endl;
+        return output;
+    }
+
+    int inputSize = input.size();
+    int outputLength = inputSize + filterSize - 1;
+
+
+    for (int i=0; i<inputLength; i++) {
+        output.push_back(filterChar(input[i]));
+    }
+
+    for (int i=inputLength; i<outputLength; i++) {
+        output.push_back(filterChar(0));
+    }
+    
     return output;
 }
 
 
 
+char filterChar(char ch) {
+    
+    writeChar(ch);
+    usleep(this->delay);
 
+    setClock(HIGH);
+    usleep(this->delay);
+
+    setClock(LOW);
+    usleep(this->delay);
+    
+    return readChar();
+
+}
